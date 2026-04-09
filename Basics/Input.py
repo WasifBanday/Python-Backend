878ffdac4bc6a2ea0407d10c83e9cbd34c958ffb
wasif_manzoor <wasifahmad679@gmail.com>
2026-04-09 14:31:53 +0530

    inpput

--- Basics/Input.py
+++ Basics/Input.py
@@ -1,1 +1,5 @@
-print("hi")
+#  Taking Two nums input fromm user and adding them ..
+fnum=int(input('Enter 1st Number '))
+snum=int(input('Enter 2nd Number '))
+result= fnum + snum
+print('Result is :-  ' , result)
