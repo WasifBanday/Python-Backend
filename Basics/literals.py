6bcde521b7c332b80290e6d514e6d676e214c26a
wasif_manzoor <wasifahmad679@gmail.com>
2026-04-09 15:21:35 +0530

    literals

---
+++ Basics/literals.py
@@ -0,0 +1,32 @@
+# The value stored in varible is called literal...
+
+# Integer 
+a = 12 # Decimal literal
+b = 0b1010 # Binary literal
+c = 0o310 # Octal literal
+d = 0x12c # Hexadecimal literal
+
+
+# Float
+float_1=10.2 # Simple float 
+float_2=1.5e2  # 1.5 * 10'2
+float_3=1.5e-3 # 1-5 * 10'-3
+
+# Complex 
+x=3.14j
+
+# String
+str_1 = 'Python'   # Single qoute
+str_2 = "Python"   # double qoute
+str_3 = """Python"""  # triple qoute
+
+# Boolean 
+a=True+1
+b=False+10
+print('a :' , a )
+print('b :' , b )
+
+
+# None
+n=None
+print(n)
