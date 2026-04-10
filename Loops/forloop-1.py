fd4efc2e0451b0b8ffd2bbba22a723980dc93197
wasif_manzoor <wasifahmad679@gmail.com>
2026-04-10 22:32:48 +0530

    For loop basics

---
+++ Loops/forloop-1.py
@@ -0,0 +1,15 @@
+# Basic for loop of identation
+for i in range(1,11):
+    print(i)
+print(' ')
+
+for i in (1,2,3,4,5):
+    print(i)
+    
+print(' ')
+for i in [1,2,4,5,6,7,3]:
+    print(i)
+
+print(' ')
+for d in 'delhi':
+    print(d)
