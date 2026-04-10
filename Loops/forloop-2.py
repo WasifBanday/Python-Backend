eba39561df43b780ab8c88ed0c3b7758816734fd
wasif_manzoor <wasifahmad679@gmail.com>
2026-04-10 22:39:50 +0530

    for loop small program

---
+++ Loops/forloop-2.py
@@ -0,0 +1,6 @@
+# Question : The current population of the town is 10000 , the population of the town is increasing 10% per year ,
+#            We have to write a program to find out the population at the end of each of the last 10 years 
+current_population=10000
+for i in range(10,0,-1):
+    print(i,'th:-' , current_population)
+    current_population=current_population-0.1*current_population
