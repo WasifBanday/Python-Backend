020923aed852ee7e32ba3ceaa2db94d029a65c65
wasif_manzoor <wasifahmad679@gmail.com>
2026-04-10 14:41:30 +0530

    Question on Operator

--- Basics/Questions.py
+++ Basics/Questions.py
@@ -1,7 +1,13 @@
 # Operator Question 
 # Find the sum of three digit number entered by user
-num1=int(input('1st no :- '))
-num2=int(input('2nd no :- '))
-num3=int(input('3rd no :- '))
-sum = num1+num2+num3
-print('Sum of entered numbers = ', sum)
+number=int(input('Enter 3 digit number:- '))
+
+a=number%10
+number=number//10
+
+b=number%10
+number=number//10
+
+c=number%10
+
+print('sum of entered 3 digits is', a+b+c)
