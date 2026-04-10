86881970382926de3a070106dd6b11ce581a6191
wasif_manzoor <wasifahmad679@gmail.com>
2026-04-10 14:55:30 +0530

    if-else

---
+++ Basics/if-else.py
@@ -0,0 +1,10 @@
+# Login program and identation 
+# email -> wasifahmad@gmail.com
+# password -> '1234'
+
+email=input('Enter email : ')
+password=input('Password : ')
+if email== 'wasifahmad@gmail.com' and password=='1234':
+    print('Welcome') 
+else:
+    print('Incorrect email and password')
