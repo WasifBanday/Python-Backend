217631a281812bdc1c53fe9eb4ded5ffae111e9c
wasif_manzoor <wasifahmad679@gmail.com>
2026-04-10 13:05:05 +0530

    Operator Queston

---
+++ Basics/Questions.py
@@ -0,0 +1,7 @@
+# Operator Question 
+# Find the sum of three digit number entered by user
+num1=int(input('1st no :- '))
+num2=int(input('2nd no :- '))
+num3=int(input('3rd no :- '))
+sum = num1+num2+num3
+print('Sum of entered numbers = ', sum)
