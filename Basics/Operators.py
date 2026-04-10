2994029e48a2476fa01048700741b1e7a76316fb
wasif_manzoor <wasifahmad679@gmail.com>
2026-04-10 12:52:30 +0530

    Operators in py

---
+++ Basics/Operators.py
@@ -0,0 +1,50 @@
+# 1 :- Arithematic operator 
+#       Addition 
+#       Subtraction 
+#       Mutiplication 
+#       Division
+#       Integer Division
+print(5//2)
+#       Modules
+print(5%2)
+#       Power of
+print(5**2)
+
+# 2 :- Realtional Operator
+#       > , < , ==  , != , >= , <= 
+
+# 3 :- Logical Operator 
+# and , or , not 
+
+# 4 :- Bitwise Operator   :---> Works on binary numbers
+
+# Bitwise &
+print(2&3)
+
+# bitwise or 
+print(2|3)
+
+# bitwise xor 
+print(2^3)
+
+# bitwise not 
+print(~3)
+
+# bitwise left Operator 
+print(4>>2)
+
+# bitwise right Operator 
+print(5<<2)
+
+
+# 5 :- Assignment Operator 
+# = , += 
+
+
+# 6 :- Membership Operator 
+
+# in
+print('D' in 'Delhi')
+
+# not in 
+print('D' not in 'Delhi')
