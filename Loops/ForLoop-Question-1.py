d038c95d98fd10c4fe1f65cd7271050bf1c12b1e
wasif_manzoor <wasifahmad679@gmail.com>
2026-04-11 15:02:57 +0530

    manipulation in code

--- Loops/ForLoop-Question-1.py
+++ Loops/ForLoop-Question-1.py
@@ -1,8 +1,9 @@
 # Find sequence sum of the terms till nth term , nth term entered by user ,, 1/1! + 2/2! + 3/3! + ... n/n!
+
 n=int(input('Enter nth term :- '))
 result=0
 fact=1
 for i in range(1,n+1):
-    fact=fact*1
+    fact=fact*i
     result=result+i/fact
 print(result)
