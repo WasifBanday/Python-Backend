7828645d05837581baae033cb5ce235baf7c09e5
wasif_manzoor <wasifahmad679@gmail.com>
2026-04-12 17:12:01 +0530

    Accessing elements of string through Indexing

---
+++ Strings/2-AccessingString.py
@@ -0,0 +1,9 @@
+# Accessing a string is one by indexing , an is are of two types [1-positive indexing and 2-negative indexing]
+
+# 1-Positive Indexing
+s='hello'
+print(s[0])
+
+# 2-negative Indexing
+s='hello'
+print(s[-1])
