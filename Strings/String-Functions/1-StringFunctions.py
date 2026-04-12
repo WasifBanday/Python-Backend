efbfd09adfa4885f1e2c59932848ddf4cc3c61a5
wasif_manzoor <wasifahmad679@gmail.com>
2026-04-12 18:09:09 +0530

    Basic Functions of string

---
+++ Strings/String-Functions/1-StringFunctions.py
@@ -0,0 +1,14 @@
+# Common functions of string
+# 1-len : Gives length of a string
+print(len('Hey man , how are you'))
+
+# 2-max : Gives the alphabet which has highest "ASCII" value
+print(max('Hey man , how are you'))
+
+# 3-min : Gives the alphabet which has lowest "ASCII" value
+print(min('Hey man , how are you'))
+
+# 4-sorted : It sorts the array in accending order , basis of their ASCII values  {spaces has the least ASCII value}
+print(sorted('Hey man , how are you'))
+# We can print this in decending order as well 
+print(sorted('Hey man , how are you', reverse=True))
