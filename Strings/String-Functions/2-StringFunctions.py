0c23cd7ad563d8b3f28d62092c7fa6972e4a3dad
wasif_manzoor <wasifahmad679@gmail.com>
2026-04-12 18:21:56 +0530

    string functions

---
+++ Strings/String-Functions/2-StringFunctions.py
@@ -0,0 +1,23 @@
+# Functions only specific to strings only 
+
+# 1- Capitalize : Capitalize the first alphabit of the first word in a string
+s='heLlo'
+print(s.capitalize())
+
+
+# 2- Title : Capitalize the first alphabit of each word in a string
+s='hello , my name is wasif'
+print(s.title())
+
+
+# 3- upper : Capitalize whole string
+s='hello'
+print(s.upper())
+
+# 4- Lower : de-Capitalize whole string
+s='hello HOW ARE you'
+print(s.lower())
+
+# 5- swapcase : capital to small , small to capital
+s='hello HOW ARE you'
+print(s.swapcase())
