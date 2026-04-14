4eeffebe2d05d790ad30220c22b740c3c90ecbff
wasif_manzoor <wasifahmad679@gmail.com>
2026-04-14 20:17:39 +0530

    List

---
+++ List/Basic.py
@@ -0,0 +1,26 @@
+# Create list 
+
+# 1:- Empty list
+l1=[]
+print(l1)
+
+# 2:- 1D list 
+l2=[1,2,3]
+print(l2)
+
+# 3:- 2D list   :--> This is an heterogeneous list because it stores multiple numbers and a list
+l3=[1,2,3,[1,2,3]]
+print(l3)
+
+# 4:- 3D list   :--> This is an homogeneous list because it contains two 2D arrays
+l4=[[[1,2],[3,4]],[[5,6],[7,8]]]
+print(l4)
+
+# 5:- Heterogeneous list
+l5=[1,True,1.24,'Hello']
+print(l5)
+
+
+# 6:-       TYPE CONVERSION   :---> [This function converts every string character into a list]
+l6=list('Hello')
+print(l6)
