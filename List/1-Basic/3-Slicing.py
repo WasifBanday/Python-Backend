1ecf8b865cd02aa2e60529fa31c30fc40b4eb3c5
wasif_manzoor <wasifahmad679@gmail.com>
2026-04-14 20:57:51 +0530

    Slicing

---
+++ List/1-Basic/3-Slicing.py
@@ -0,0 +1,7 @@
+# Slicing :- Also a method of accessing items in an list but it accesses multiple items in one go .. 
+L=[1,2,3,5,6]
+# Print first 3 items in this list
+print(L[:3])
+
+# Print last 3 items in this list
+print(L[-3:])
