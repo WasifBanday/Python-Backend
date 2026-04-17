def6acd6bc42218a4f82ddc3b5ae0998906c3c9a
wasif_manzoor <wasifahmad679@gmail.com>
2026-04-17 11:51:07 +0530

    Tuple Today

---
+++ Tuple/1-Basic.py
@@ -0,0 +1,30 @@
+# Tuple in Python is similar to list , the only difference is that , we can not change elements of an tuble after decleration , where as we can change the elements of a set 
+# Tuple is immutable list 
+# Characteristics 
+# 1.Ordered
+# 2.Unchangeable 
+# 3. Allow Duplicates 
+
+# Creating Tuple 
+# Empty Tuple 
+T1=()
+print(type(T1))
+
+# Creating a tuple with a single item 
+T2=(3,)  # , is important if we create a tuple with single item 
+print(T2)
+
+# Homogeneous Tuple
+T3=(1,2,3,45,5,6,77,4,89,98)
+print(T3)
+
+# Hetrogeneous Tuple
+T4=(1,True,'Hello')
+
+# Tuple
+T5=(1,2,3,(123,234,345))
+print(T5)
+
+# Using Type Conversion 
+T6=tuple('hello')
+print(T6)
