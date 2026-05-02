50d3c9fc0afea2f2e28c1f2f54c2dc2586e9a543
wasif_manzoor <wasifahmad679@gmail.com>
2026-05-02 21:49:42 +0530

    HOF

---
+++ FUNCTIONN/HigherOrderFunctions.py
@@ -0,0 +1,9 @@
+# The type of functions which returns another function , or which receives another function as input 
+
+def transform (f,l):
+    output=[]
+    for i in l :
+        output.append(f(i))
+    print(output)
+l=[1,2,3,4,5]
+transform(lambda x : x**2 , l)
