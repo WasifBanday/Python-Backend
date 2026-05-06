23d06404e7acbbc6377463b1b05c4ede50403d94
wasif_manzoor <wasifahmad679@gmail.com>
2026-05-06 12:27:58 +0530

    Map function example

--- FUNCTIONN/9-MapFunction.py
+++ FUNCTIONN/9-MapFunction.py
@@ -7,3 +7,23 @@ print(list(map(lambda x:x**2 , [1,2,3,4,5])))
 # Example 2 : Odd / Even 
 l=[1,2,3,4,5,6,7]
 print(list(map(lambda x:'even' if x % 2==0 else 'odd' , l)))
+
+# Example 3 : Fetch names from a list of dict
+users=[
+    {
+        'name' : 'Rahul',
+        'age': 20,
+        'gender':'male'
+    },
+    {
+        'name' : 'wasif',
+        'age': 21,
+        'gender':'male'
+    },
+    {
+        'name' : 'Aisha',
+        'age': 19,
+        'gender':'female'
+    }
+]
+print(list(map(lambda users:users['gender'],users)))
