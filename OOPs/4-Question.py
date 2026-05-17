11005e5a3f1caba46da522027cb47871cd96e3b5
wasif_manzoor <wasifahmad679@gmail.com>
2026-05-17 17:02:35 +0530

    Cordinate geometry example

---
+++ OOPs/4-Question.py
@@ -0,0 +1,24 @@
+# Write OOP classes to handle the following scenarios:
+#   1: A user can create and view 2D coordinates
+#   2: A user can find out the distance between 2 coordinates
+#   3: A user can find find the distance of a coordinate from origin
+#   4: A user can check if a point lies on a given line
+#   5: A user can find the distance between a given 2D point and a given line 
+
+# 1: 
+class point :
+    def __init__(self,x,y):
+        self.x_cod=x
+        self.y_cod=y
+        
+    def __str__(self):
+        return '<{},{}>'.format(self.x_cod,self.y_cod)
+# p=point(1,1)
+# print(p)
+
+# 2: distance formula between two coordinates = ((x2​−x1​)**2 + (y2​−y1​)**2 ) ** 0.5 
+    def euclidean_distance(self,other):
+        return ((self.x_cod-other.x_cod)**2 + (self.y_cod-other.y_cod)**2) ** 0.5
+p1=point(0,0)
+p2=point(10,10)
+print(p1.euclidean_distance(p2))
