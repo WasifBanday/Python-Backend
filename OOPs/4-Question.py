08596de018c3f0e3986ca39f4815819a6a8df452
wasif_manzoor <wasifahmad679@gmail.com>
2026-05-18 22:35:08 +0530

    Shortest distance formula

--- OOPs/4-Question.py
+++ OOPs/4-Question.py
@@ -26,8 +26,8 @@ class point :
 # 3: Distance formula from origin to point  d=( x2+y2 ) ** 0.5 
     def distance_from_origin(self):
         return (self.x_cod**2 + self.y_cod**2)** 0.5
-p=point(0,0)
-print(p.distance_from_origin())
+# p=point(0,0)
+# print(p.distance_from_origin())
 
 # 4: Creating new class for checking if the point lies on a plane or not
 class Line:
@@ -43,8 +43,15 @@ class Line:
         else :
             return 'Does not lies on line'
     
-L1=Line(1,1,-2)
-P1=point(1,2)
-print(L1)
-print(P1)
-print(L1.point_on_line(P1))
+# L1=Line(1,1,-2)
+# P1=point(1,2)
+# print(L1)
+# print(P1)
+# print(L1.point_on_line(P1))
+
+# 5: Formula for shortest distance between line and point   | Ax + By + C | / (A**2 + B**2) ** 0.5 
+    def shortest_distance(Line,point):
+        return abs(Line.A * point.x_cod + Line.B * point.y_cod + Line.C) / (Line.A**2 + Line.B**2) ** 0.5
+# L1=Line(1,1,2)
+# p1=point(1,1)
+# print(L1.shortest_distance(p1))
