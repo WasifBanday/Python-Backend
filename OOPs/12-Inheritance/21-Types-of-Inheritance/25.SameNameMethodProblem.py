9a0f08a89f1012509d580f9bc899e40bf8d21510
wasif_manzoor <wasifahmad679@gmail.com>
2026-09-10 21:45:43 +0530

    Method resolution order

---
+++ OOPs/12-Inheritance/21-Types-of-Inheritance/25.SameNameMethodProblem.py
@@ -0,0 +1,18 @@
+class phone:
+    def __init__(self,price,brand,camera):
+        print("Inside phone constructor")
+        self.price=price
+        self.brand=brand
+        self.camera=camera
+    def buy(self):
+        print("Buying a phone")
+
+class product:
+    def buy(self):
+        print("Buying a product")
+
+# Method Resolution Order
+class smartphone(product,phone): # Here on calling the ' s.buy() '  products buy() will be called because its 1st in receiving
+    pass
+s=smartphone(2000,"apple","12px")
+s.buy()
